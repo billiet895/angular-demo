671f7e3b11e962a5a08eded555e697cf1257c18e
George Katsanos <gkatsanos@gmail.com>
2014-09-13 17:52:13 +0000

    Minor fixes

--- app/js/services.js
+++ app/js/services.js
@@ -3,7 +3,7 @@ var chatServices = angular.module('chatServices', ['ngResource']);
 chatServices.factory('Chat',['$resource',
   function($resource){
     return $resource('sample-chat-data.json', {}, {
-      query: {method:'GET', params:{}, isArray:true}
+      query: {method:'GET', params:{}, isArray:true, cache: true}
     });
   }
 ]);
